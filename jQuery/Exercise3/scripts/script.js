1ba56380f3b60b60daff4a9fedcf9a6ba8e49ba4
quangnt <quang217@gmail.com>
2019-01-10 18:10:05 +0700

    Exercise 3 jQuery

---
+++ jQuery/Exercise3/scripts/script.js
@@ -0,0 +1,35 @@
+var filename = "leaves";
+var formatfile = ".png";
+var index = 0;
+
+setInterval(function(){
+    //random image
+    var rd = Math.floor(Math.random() * 5 + 1);
+    //random falltime of leaf 
+    var falltime = Math.floor(Math.random() * 10 + 10);
+    //random rotate direction
+    var rotate = Math.random() > 0.5 ? 1 : -1;
+    //random rotation angle
+    var angle = Math.floor(Math.random() * 720 + 360);
+    //random begin X position
+    var startX = Math.floor(Math.random() * 1500);
+    //random end X position
+    var endX = Math.floor(Math.random() * 1500);
+    //create leaf
+    var leaf = $('<img/>').attr({
+        "id": index,
+        "src": "images/" + filename + rd + formatfile,
+        "alt": filename
+    }).appendTo("#container");
+    index++;
+    //let fall
+    TweenMax.fromTo($("#" + leaf.attr("id")),
+    falltime, 
+    {x: startX, y:-100},
+    {x: endX, y:700, onComplete: function() {
+        $(leaf).remove();
+    }});
+    TweenMax.to("#" + leaf.attr("id"),
+    falltime,
+    {rotation: rotate * angle, rotationY: rotate * angle});
+}, 800);
