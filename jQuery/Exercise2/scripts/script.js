96300d9a031e7535692a58f9c2f7dd68156190a8
quangnt <quang217@gmail.com>
2019-01-10 10:16:12 +0700

    fix

--- jQuery/Exercise2/scripts/script.js
+++ jQuery/Exercise2/scripts/script.js
@@ -1,115 +1,125 @@
-// $(".slideshow img:gt(0)").hide();
-
-// setInterval(function() {
-//     $(".slideshow :first-child").animate({
-//         width: "toggle"
-//     })
-//     .next("img").animate({
-//         width: "toggle"
-//     }).end().appendTo(".slideshow");
-// }, 3000);
-
 var index = 1;
 var width = 648;
 var opac = 0.5;
-
-$(document).ready(function() {
-    $("#arrowleft").click(function(){
-        var i;
-        setAnimate("#item_nav-", index, 1);
-        index++;
-        if (index > 5) {
-            index = 1;
-            for (i = 1; i <= 5; i++) {
-                $("#item-" + i).animate({
-                    left: "+=" + width * 4 + "px"
-                });
-            }
-        } else {
-            for (i = 1; i <= 5; i++) {
-                $("#item-" + i).animate({
-                    left: "-=" + width + "px"
-                });
-            }
-        }  
-        setAnimate("#item_nav-", index, opac);
-    });
-});
-
-$(document).ready(function() {
-    $("#arrowright").click(function(){
-        var i;
-        setAnimate("#item_nav-", index, 1);
-        index--;
-        if (index < 1) {
-            index = 5;
-            for (i = 5; i >= 1; i--) {
-                $("#item-" + i).animate({
-                    left: "-=" + width * 4 + "px"
-                });
-            }
-        } else {
-            for (i = 5; i >= 1; i--) {
-                $("#item-" + i).animate({
-                    left: "+=" + width + "px"
-                });
-            }
-        }  
-        setAnimate("#item_nav-", index, opac);
-    });
-
-    $(".item_nav").click(function(){
-        var item = $(this).attr("id");
-        var choice = item.split("-");
-        move(index, choice[1]);
-        setAnimate("#item_nav-", choice[1], opac);
-        setAnimate("#item_nav-", index, 1);
-        index = choice[1];
-    });
-});
-
+var interval;
+var timer = function(){
+    interval = setInterval(function() {
+        $("#arrowright").click();
+    }, 5000);;
+}
+var countSlide;
+//window load
 window.onload = WindowEventLoaded
-
 function WindowEventLoaded() {
     $("#item-1").show();
-    $("#item_nav-1").css("opacity", 0.7);
+    $("#item_nav-1").css("opacity", opac);
+    countSlide = $("#slideshow-js li").length;
     var i;
-    for (i = 1; i <= 5; i++) {
+    for (i = 1; i <= countSlide; i++) {
         $("#item-" + i).animate({
             left: "+=" + width * (i - 1) + "px"
         });
     }
-    for (i = 2; i <= 5; i++) {
+    for (i = 2; i <= countSlide; i++) {
         $("#item-" + i).show(100);
     }
+    timer();
 }
 
+//set click event
+$(document).ready(function() {
+    //click arrow left
+    $("#arrowleft").click(function(){
+        setAnimateOpacity("#item_nav-", index, 1);
+        index--;
+        if (index < 1) {
+            index = countSlide;
+            moveLeft("#item-", width * (countSlide - 1));
+        } else {
+            moveRight("#item-", width);
+        }  
+        setAnimateOpacity("#item_nav-", index, opac);
+        clearInterval(interval);
+        timer();
+    });
+    //click arrow right
+    $("#arrowright").click(function(){
+        setAnimateOpacity("#item_nav-", index, 1);
+        index++;
+        if (index > countSlide) {
+            index = 1;
+            moveRight("#item-", width * (countSlide - 1));
+        } else {
+            moveLeft("#item-", width);
+        }  
+        setAnimateOpacity("#item_nav-", index, opac);
+        clearInterval(interval);
+        timer();
+    });
+    //click item nav
+    $(".item_nav").click(function(){
+        var item = $(this).attr("id");
+        var choice = item.split("-");
+        move(index, choice[1]);
+        setAnimateOpacity("#item_nav-", choice[1], opac);
+        setAnimateOpacity("#item_nav-", index, 1);
+        index = choice[1];
+        clearInterval(interval);
+        timer();
+    });
+});
+/**
+ * move slide in slideshow when click item nav
+ * @param {index current item} indexA 
+ * @param {index item click} indexB 
+ */
 function move(indexA, indexB) {
     if (indexA == indexB) {
         return;
     } else if (indexA > indexB) {
         var offset = indexA - indexB;
         var distance = width * offset;
-        var i;
-        for (i = 1; i <= 5; i++) {
-            $("#item-" + i).animate({
-                left: "+=" + distance + "px"
-            });
-        }
+        moveRight("#item-", distance);
     } else {
         var offset = indexB - indexA;
         var distance = width * offset;
-        var i;
-        for (i = 1; i <= 5; i++) {
-            $("#item-" + i).animate({
-                left: "-=" + distance + "px"
-            });
-        }
+        moveLeft("#item-", distance);
     }
 }
-
-function setAnimate(item, index, value) {
-    $(item + index).animate({
+/**
+ * slide left
+ * @param {prefix id of item in slideshow} obj 
+ * @param {distance move} distance 
+ */
+function moveLeft(obj, distance) {
+    var i;
+    for (i = 1; i <= countSlide; i++) {
+        $(obj + i).animate({
+            left: "-=" + distance + "px"
+        });
+    }
+}
+/**
+ * slide right
+ * @param {prefix id of item in slideshow} obj 
+ * @param {distance move} distance 
+ */
+function moveRight(obj, distance) {
+    var i;
+    for (i = 1; i <= countSlide; i++) {
+        $(obj + i).animate({
+            left: "+=" + distance + "px"
+        });
+    }
+}
+/**
+ * set opacity of object
+ * @param {prefix id of item in nav} obj 
+ * @param {index item in nav} index 
+ * @param {value of opacity} value 
+ */
+function setAnimateOpacity(obj, index, value) {
+    $(obj + index).animate({
         opacity: value
     });
 }
