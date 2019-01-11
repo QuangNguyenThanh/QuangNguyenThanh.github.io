05ac78ea72ac57a146ea641e0e3caf6b2b61e601
quangnt <quang217@gmail.com>
2019-01-11 11:19:59 +0700

    fix exercise 2 JS

--- Javascript/Exercise2_JS/scripts/script.js
+++ Javascript/Exercise2_JS/scripts/script.js
@@ -1,7 +1,13 @@
 var check = false;
-//Create Calendar
+var day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+//create Calendar
 function createTable() {
-    if (!check) {
+    if (check) {
+        deleteElement("container", "table");
+        check = false;
+    } else {
+        check = true;     
         var container = document.getElementById("container");
         var table = document.createElement("table");
         table.setAttribute("id", "table");
@@ -13,15 +19,12 @@ function createTable() {
         var currentMonth = today.getMonth();
         var currentYear = today.getFullYear();
         var dayOfMonth = daysInMonth(currentMonth, currentYear);
-    
         var firstDay = new Date(currentYear, currentMonth, 1).getDay();
-    
         var count = 1;
         for (i = 0; i < 6; i++) {
             var row = document.createElement("tr");
             for (j = 0; j < 7; j++) {
-                var cell = document.createElement("th");
-                cell.setAttribute("id", "cell" + (i * 7 + j));
+                var cell = document.createElement("td");
                 if (i === 0 && j < firstDay) {
                     cell.setAttribute("class", "not_day-js");                
                     row.appendChild(cell);
@@ -41,77 +44,95 @@ function createTable() {
             }
             table.appendChild(row);
         }
-    
         container.appendChild(table);
     
         var month = document.getElementById("month-js");
         var year = document.getElementById("year-js");
-        updateMonth(month, today);
-        updateYear(year, today);
-    
-        month.onchange = year.onchange = function() {
-            selectCbo(year.options[year.selectedIndex].value, 
-                month.options[month.selectedIndex].value, 
-                daysInMonth(month.options[month.selectedIndex].value, 
-                    year.options[year.selectedIndex].value));
-        }
-    
         var preMonth = document.getElementById("preMonth-js");
         var preYear = document.getElementById("preYear-js");
         var nextMonth = document.getElementById("nextMonth-js");
         var nextYear = document.getElementById("nextYear-js");
-        check = true;
-        //set event click previous month
-        preMonth.onclick = function() {
-            selectCbo(year.options[year.selectedIndex].value, 
-                parseInt(month.options[month.selectedIndex].value) - 1, 
-                daysInMonth(parseInt(month.options[month.selectedIndex].value) - 1, 
-                    year.options[year.selectedIndex].value));
-            var newDate = new Date();
-            newDate.setFullYear(year.options[year.selectedIndex].value, 
-                parseInt(month.options[month.selectedIndex].value) - 1,
-             1);
-            updateMonth(month, newDate);
-            updateYear(year, newDate);
-        }
-        //set event click previous year
-        preYear.onclick = function() {
-            selectCbo(parseInt(year.options[year.selectedIndex].value) - 1, 
-                month.options[month.selectedIndex].value, 
-                daysInMonth(month.options[month.selectedIndex].value, 
-                    parseInt(year.options[year.selectedIndex].value) - 1));
-            var newDate = new Date();
-            newDate.setFullYear(parseInt(year.options[year.selectedIndex].value) - 1, 
-            month.options[month.selectedIndex].value,
-             1);
-            updateYear(year, newDate);
-        }
-        //set event click next month
-        nextMonth.onclick = function() {
-            selectCbo(year.options[year.selectedIndex].value, 
-                parseInt(month.options[month.selectedIndex].value) + 1, 
-                daysInMonth(parseInt(month.options[month.selectedIndex].value) + 1, 
-                    year.options[year.selectedIndex].value));
-            var newDate = new Date();
-            newDate.setFullYear(year.options[year.selectedIndex].value, 
-                parseInt(month.options[month.selectedIndex].value) + 1,
-             1);
-            updateMonth(month, newDate);
-            updateYear(year, newDate);
-        }
-        //set event click next year
-        nextYear.onclick = function() {
-            selectCbo(parseInt(year.options[year.selectedIndex].value) + 1, 
-                month.options[month.selectedIndex].value, 
-                daysInMonth(month.options[month.selectedIndex].value, 
-                    parseInt(year.options[year.selectedIndex].value) + 1));
-            var newDate = new Date();
-            newDate.setFullYear(parseInt(year.options[year.selectedIndex].value) + 1, 
-            month.options[month.selectedIndex].value,
-             1);
-            updateYear(year, newDate);
-        }
-
+        
+        updateMonth(month, today);
+        updateYear(year, today);
+        onChangeSelect(month, year);
+        onClickPreYear(preYear, month, year);
+        onClickPreMonth(preMonth, month, year);
+        onClickNextMonth(nextMonth, month, year);
+        onClickNextYear(nextYear, month, year);
+    }
+}
+//Delete element
+function deleteElement(parent, child) {
+    var parent = document.getElementById(parent);
+    var child = document.getElementById(child);
+    parent.removeChild(child);
+}
+//set event click previous year
+function onClickPreYear(preYear, month, year) {
+    preYear.onclick = function() {
+        selectCbo(parseInt(year.options[year.selectedIndex].value) - 1, 
+            month.options[month.selectedIndex].value, 
+            daysInMonth(month.options[month.selectedIndex].value, 
+                parseInt(year.options[year.selectedIndex].value) - 1));
+        var newDate = new Date();
+        newDate.setFullYear(parseInt(year.options[year.selectedIndex].value) - 1, 
+        month.options[month.selectedIndex].value,
+            1);
+        updateYear(year, newDate);
+    }
+}
+//set event click previous month
+function onClickPreMonth(preMonth, month, year) {
+    preMonth.onclick = function() {
+        selectCbo(year.options[year.selectedIndex].value, 
+            parseInt(month.options[month.selectedIndex].value) - 1, 
+            daysInMonth(parseInt(month.options[month.selectedIndex].value) - 1, 
+                year.options[year.selectedIndex].value));
+        var newDate = new Date();
+        newDate.setFullYear(year.options[year.selectedIndex].value, 
+            parseInt(month.options[month.selectedIndex].value) - 1,
+        1);
+        updateMonth(month, newDate);
+        updateYear(year, newDate);
+    }
+}
+//set event click next month
+function onClickNextMonth(nextMonth, month, year) {
+    nextMonth.onclick = function() {
+        selectCbo(year.options[year.selectedIndex].value, 
+            parseInt(month.options[month.selectedIndex].value) + 1, 
+            daysInMonth(parseInt(month.options[month.selectedIndex].value) + 1, 
+                year.options[year.selectedIndex].value));
+        var newDate = new Date();
+        newDate.setFullYear(year.options[year.selectedIndex].value, 
+            parseInt(month.options[month.selectedIndex].value) + 1,
+        1);
+        updateMonth(month, newDate);
+        updateYear(year, newDate);
+    }
+}
+//set event click next year
+function onClickNextYear(nextYear, month, year) {
+    nextYear.onclick = function() {
+        selectCbo(parseInt(year.options[year.selectedIndex].value) + 1, 
+            month.options[month.selectedIndex].value, 
+            daysInMonth(month.options[month.selectedIndex].value, 
+                parseInt(year.options[year.selectedIndex].value) + 1));
+        var newDate = new Date();
+        newDate.setFullYear(parseInt(year.options[year.selectedIndex].value) + 1, 
+        month.options[month.selectedIndex].value,
+        1);
+        updateYear(year, newDate);
+    }
+}
+//set event on change combobox
+function onChangeSelect(month, year) {
+    month.onchange = year.onchange = function() {
+        selectCbo(year.options[year.selectedIndex].value, 
+            month.options[month.selectedIndex].value, 
+            daysInMonth(month.options[month.selectedIndex].value, 
+                year.options[year.selectedIndex].value));
     }
 }
 //Get day choice in calendar and show in text box
@@ -122,12 +143,9 @@ function choiceDay(day) {
     var year = document.getElementById("year-js");
     var m = month.options[month.selectedIndex].value;
     var y = year.options[year.selectedIndex].value;
-
     document.getElementById("calendar-js").value = day.padStart(2, '0') + "/" + (parseInt(m) + 1 + "").padStart(2, '0') + "/" + y;
-
-    var container = document.getElementById("container");
-    var table = document.getElementById("table");
-    container.removeChild(table);
+    //remove table calendar
+    deleteElement("container", "table");
     check = false;
 }
 //Get days in month
@@ -140,28 +158,23 @@ function selectCbo(year, month, dayOfMonth) {
     date.setFullYear(year, month, 1);
     var firstDay = date.getDay();
     var i;
-    var j;
     var count = 1;
-    for (i = 0; i < 6; i++) {
-        for (j = 0; j < 7; j++) {
-            var cell = document.getElementById("cell" + (i * 7 + j));
-
-            if ((i * 7 + j) < firstDay) {
-                cell.setAttribute("class", "not_day-js");                
-                cell.innerHTML = "";
-                continue;
-            }
-            if (count <= dayOfMonth) {
-                cell.setAttribute("class", "day-js");
-                cell.classList.add("choice-js");
-                cell.innerHTML = count;
-                cell.onclick = function() {choiceDay(this.innerHTML)};
-                count++;       
-            }
-            else {
-                cell.innerHTML = "";
-                cell.setAttribute("class", "not_day-js");
-            }
+    var listday = document.getElementById("table").getElementsByTagName("td");
+    for (i = 0; i < 42; i++) {
+        if (i < firstDay) {
+            listday[i].setAttribute("class", "not_day-js");                
+            listday[i].innerHTML = "";
+            continue;
+        }
+        if (count <= dayOfMonth) {
+            listday[i].setAttribute("class", "day-js");
+            listday[i].classList.add("choice-js");
+            listday[i].innerHTML = count;
+            listday[i].onclick = function() {choiceDay(this.innerHTML)};
+            count++;       
+        } else {
+            listday[i].innerHTML = "";
+            listday[i].setAttribute("class", "not_day-js");
         }
     }
 }
@@ -177,8 +190,6 @@ function updateMonth(month, date) {
 function createRowDate(table) {
     var rowDate = document.createElement("tr");
     var i;
-    var day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
     for (i = 0; i < 7; i++) {
         var cellDate = document.createElement("th");
         cellDate.setAttribute("class", "date-js");
@@ -203,14 +214,12 @@ function createNav(table) {
     nextMonth.setAttribute("id", "nextMonth-js");
     var nextYear = document.createElement("th");
     nextYear.setAttribute("id", "nextYear-js");
-
     preYear.innerHTML = "<<";
     preMonth.innerHTML = "<";
     nextMonth.innerHTML = ">";
     nextYear.innerHTML = ">>";
 
     var i;
-    var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     var cboM = document.createElement("select");
     cboM.setAttribute("id", "month-js");
     for (i = 0; i < 12; i++) {
@@ -219,7 +228,6 @@ function createNav(table) {
         t.innerHTML = month[i];
         cboM.appendChild(t);
     }
-
     var cboY = document.createElement("select");
     cboY.setAttribute("id", "year-js");
     for (i = 1900; i < 2100; i++) {
@@ -231,7 +239,6 @@ function createNav(table) {
 
     cboMonth.appendChild(cboM);
     cboYear.appendChild(cboY);
-
     rowNav.appendChild(preYear);
     rowNav.appendChild(preMonth);
     rowNav.appendChild(cboMonth);
