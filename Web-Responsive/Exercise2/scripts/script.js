4a8b6849d7c31ca6577a2952254f25a1ea604da1
quangnt <quang217@gmail.com>
2019-01-22 13:44:23 +0700

    fix

--- Web-Responsive/Exercise2/scripts/script.js
+++ Web-Responsive/Exercise2/scripts/script.js
@@ -3,7 +3,6 @@ var onScroll = false;
 $(window).scroll(function() {
     if(Math.floor($(window).scrollTop() + $(window).height()) >= $(document).height() - 1) {
         if (!onScroll) {
-            console.log(onScroll);
             onScroll = true;
             $(".blood").show(); //show blood
             //change image horror
@@ -18,18 +17,20 @@ $(window).scroll(function() {
             $(".time, .intro").addClass("js-red__area");
             //scroll to top
             $("html, body").animate({ scrollTop: 0 }, 5000, function() {
-                $(".blood").hide(); //hide blood
-                //change image normal
-                $(".items").each(function() {
-                    $(this).attr("src", $(this).attr("src").replace("_hide.png", ".png"));
-                });
-                //remove css horror
-                $(".container").css("background-color", "white");
-                $(".title, .small__title, .time").removeClass("js-redtext__horror");
-                $(".col-1").removeClass("js-hr__horror");
-                $(".normal__text, .small__text").removeClass("js-whitetext__horror");
-                $(".time, .intro").removeClass("js-red__area");
-                onScroll = false;
+                setTimeout(() => {
+                    $(".blood").hide(); //hide blood
+                    //change image normal
+                    $(".items").each(function() {
+                        $(this).attr("src", $(this).attr("src").replace("_hide.png", ".png"));
+                    });
+                    //remove css horror
+                    $(".container").css("background-color", "white");
+                    $(".title, .small__title, .time").removeClass("js-redtext__horror");
+                    $(".col-1").removeClass("js-hr__horror");
+                    $(".normal__text, .small__text").removeClass("js-whitetext__horror");
+                    $(".time, .intro").removeClass("js-red__area");
+                    onScroll = false;
+                }, 1000);
             });
         }
     }
